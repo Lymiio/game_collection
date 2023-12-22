eff6f72696606b8c9d674272fa9add7d67ae9bdc
Corentyn Hayer <115353585+Corentyn-dev@users.noreply.github.com>
2023-12-22 09:49:35 +0100

    Create global.php
    Global code for navbar and footer (structure)

---
+++ views/global.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Game Collection | Home</title>
+    <link rel="stylesheet" href="../assets/css/global.css">
+</head>
+<body>
+
+    <header>
+        <img src="../assets/img/" alt="Logo du site">
+        <nav>
+            <ul class="right-items">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="games.php">Games</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <?= $content ?>
+    </main>
+
+    <footer>
+        <p>Game Collection &copy; 2020</p>
+    </footer>
+    
+</body>
+</html>
