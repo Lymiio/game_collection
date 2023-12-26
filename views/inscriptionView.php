5142431e857e0c640c1fba5f27e3a7d4f13c19cf
CocoCoding <115353585+Corentyn-dev@users.noreply.github.com>
2023-12-26 12:17:18 +0100

    Update inscription files
    Adding style to inscriptionView.php
    Changing the structure of inscriptionView.php (removed errors)

--- views/inscriptionView.php
+++ views/inscriptionView.php
@@ -1,25 +1,51 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/global.css">
+    <link rel="stylesheet" href="../assets/css/inscription.css">
     <title>Inscription</title>
 </head>
+
 <body>
-    <h1>Inscription</h1>
-    <form action="register.php" method="POST">
-        <p>Nom :</p>
-        <input type="text" name="nom" id="nom">
-        <p>Prénom :</p>
-        <input type="text" name="prenom" id="prenom">
-        <p>Email :</p>
-        <input type="email" name="email" id="email">
-        <p>Mot de passe :</p>
-        <input type="password" name="mdp" id="mdp">
-        <p>Confirmation du mot de passe :</p>
-        <input type="password" name="mdp2" id="mdp2">
-        <br>
-        <input type="submit" value="S'inscrire">
-    </form>
+
+    <main>
+
+        <div class="container">
+
+            <h1 class="heading1">
+                Inscription
+            </h1>
+
+            <form action="">
+
+                <label for="name">Nom :</label>
+                <input type="text" require>
+                <label for="firstname">Prénom :</label>
+                <input type="text" require>
+                <label for="email">Email :</label>
+                <input type="email" require>
+                <label for="password">Mot de passe :</label>
+                <input type="password" name="password" id="password" require>
+                <label for="password">Confirmer le mot de passe :</label>
+                <input type="password" name="password" id="password" require>
+
+                <button type="button">S'inscrire</button>
+
+            </form>
+
+            <a href="connexionView.php">Se connecter</a>
+
+        </div>
+
+    </main>
+
+    <footer>
+        <p>Game Collection &copy; 2023 - Tous Droits Réservés</p>
+    </footer>
+
 </body>
+
 </html>
