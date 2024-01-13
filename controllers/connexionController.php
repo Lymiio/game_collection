a3afd1358c9a68a5d1d1168b467c110e94b57a12
Corentyn Hayer <115353585+Corentyn-dev@users.noreply.github.com>
2024-01-13 13:31:26 +0100

    Update connection files
    Adding better comments, and adding the Prettier structure for a better indentation, form, etc...

--- controllers/connexionController.php
+++ controllers/connexionController.php
@@ -1,46 +1,43 @@
 <?php
 session_start();
 
-// Vérification si le formulaire a été soumis
+// Verification of the form submission
 if (isset($_POST['email']) && isset($_POST['password'])) {
-    // Connexion à la base de données
+    // Connection to database
     $host = 'localhost';
     $dbname = 'game_collection';
     $user = 'root';
     $password = '';
 
-    $connexion = mysqli_connect($host, $user, $password, $dbname);
+    $conn = mysqli_connect($host, $user, $password, $dbname);
 
-    // Vérifier la connexion
-    if (!$connexion) {
+    // Verify connection
+    if (!$conn) {
         die("La connexion à la base de données a échoué : " . mysqli_connect_error());
     }
 
-    // Préparation de la requête SQL pour vérifier les informations d'identification
-    $email = mysqli_real_escape_string($connexion, $_POST['email']);
+    // Prepare the query
+    $email = mysqli_real_escape_string($conn, $_POST['email']);
     $query = "SELECT * FROM UTILISATEUR WHERE email_utilisateur = '$email'";
-    $result = mysqli_query($connexion, $query);
+    $result = mysqli_query($conn, $query);
 
     if ($result) {
         $user = mysqli_fetch_assoc($result);
 
-        // Vérification du mot de passe
+        // Verify password
         if ($user && password_verify($_POST['password'], $user['mot_de_passe_utilisateur'])) {
-            // Authentification réussie
             $_SESSION['user_id'] = $user['id_utilisateur'];
 
-            // Redirection vers la page suivante (par exemple, dashboard.php)
-            header('Location: ../views/global.php');
+            // Redirection to main page
+            header('Location: ../views/homeView.php');
             exit();
         } else {
-            // Authentification échouée
             echo 'Identifiants incorrects';
         }
     } else {
-        echo "Erreur lors de la requête : " . mysqli_error($connexion);
+        echo "Erreur lors de la requête : " . mysqli_error($conn);
     }
 
-    // Fermer la connexion
-    mysqli_close($connexion);
+    // Close connection
+    mysqli_close($conn);
 }
-?>
--- views/connexionView.php
+++ views/connexionView.php
@@ -3,10 +3,11 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Connexion</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/global.css">
     <link rel="stylesheet" href="../assets/css/connection.css">
+    <link rel="shortcut icon" href="../assets/img/logo.png" type="image/x-icon">
+    <title>Game Collection | Connexion</title>
 </head>
 
 <body>
@@ -18,8 +19,8 @@
 
             <form action="../controllers/connexionController.php" method="POST">
                 <label for="email">Email :</label>
-                <input type="text" name="email" required>
-                
+                <input type="text" name="email" id="email" required>
+
                 <label for="password">Mot de passe :</label>
                 <input type="password" name="password" id="password" required>
 
