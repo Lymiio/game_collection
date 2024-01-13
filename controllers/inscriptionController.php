cc97ccade9b8dbb8b13df20917972cd57e4f9912
Corentyn Hayer <115353585+Corentyn-dev@users.noreply.github.com>
2024-01-13 11:56:06 +0100

    Update inscriptionController.php
    Remove bad name for variables.
    Adding Prettier structure for better indentation, form, etc...

--- controllers/inscriptionController.php
+++ controllers/inscriptionController.php
@@ -1,55 +1,54 @@
 <?php
-// Connexion à la base de données (à remplacer par vos informations)
+// Connection to database
 $host = 'localhost';
 $dbname = 'game_collection';
 $user = 'root';
 $password = '';
 
-$connexion = mysqli_connect($host, $user, $password, $dbname);
+$conn = mysqli_connect($host, $user, $password, $dbname);
 
-// Vérifier la connexion
-if (!$connexion) {
+// Verify connection
+if (!$conn) {
     die("La connexion à la base de données a échoué : " . mysqli_connect_error());
 }
 
-// Récupérer les données du formulaire
+// Get data from form
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
 $email = $_POST['email'];
-$mot_de_passe = $_POST['mot_de_passe'];
-$confirmation_mot_de_passe = $_POST['confirmation_mot_de_passe'];
+$password = $_POST['password'];
+$confirm__password = $_POST['confirm__password'];
 
-// Vérifier si les mots de passe correspondent
-if ($mot_de_passe !== $confirmation_mot_de_passe) {
+// Verify corresponding passwords
+if ($password !== $confirm__password) {
     echo "Erreur : Les mots de passe ne correspondent pas.";
 } else {
-    try{
-        // Hachage du mot de passe
-        $mot_de_passe_hache = password_hash($mot_de_passe, PASSWORD_DEFAULT);
+    try {
+        // Hash password
+        $hash__password = password_hash($password, PASSWORD_DEFAULT);
 
-        // Vérifier si l'email existe déjà dans la base de données
-        $verification_requete = "SELECT * FROM UTILISATEUR WHERE email_utilisateur='$email'";
-        $resultat_verification = mysqli_query($connexion, $verification_requete);
+        // Prepare request
+        $query = "SELECT * FROM UTILISATEUR WHERE email_utilisateur='$email'";
+        $result__query = mysqli_query($conn, $query);
 
-        if (mysqli_num_rows($resultat_verification) > 0) {
-            // L'email existe déjà, renvoyer une erreur ou rediriger vers une page d'erreur
+        // Check if email already exists
+        if (mysqli_num_rows($result__query) > 0) {
             echo "Erreur : Cet email est déjà enregistré.";
         } else {
-            // L'email n'existe pas, procéder à l'inscription
-            $requete = "INSERT INTO UTILISATEUR (nom_utilisateur, prenom_utilisateur, email_utilisateur, mot_de_passe_utilisateur) VALUES ('$nom', '$prenom', '$email', '$mot_de_passe_hache')";
+            // Email doesn't exist, insert data into database
+            $insert__query = "INSERT INTO UTILISATEUR (nom_utilisateur, prenom_utilisateur, email_utilisateur, password_utilisateur) VALUES ('$nom', '$prenom', '$email', '$hash__password')";
 
-            if (mysqli_query($connexion, $requete)) {
+            if (mysqli_query($conn, $insert__query)) {
                 header('Location: ../views/connexionView.php');
-                exit(); // Assurez-vous d'ajouter exit() après la redirection
+                exit();
             } else {
-                echo "Erreur lors de l'inscription : " . mysqli_error($connexion);
+                echo "Erreur lors de l'inscription : " . mysqli_error($conn);
             }
         }
     } catch (PDOException $e) {
-        die($e -> getMessage());
+        die($e->getMessage());
     }
 }
 
-// Fermer la connexion
+// Close connection
 mysqli_close($connexion);
-?>
