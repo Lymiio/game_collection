19d4ba7431697be9ba90db0d674042ff6e649f9a
Timéo Cherbonnel <timeocherbonnel@gmail.com>
2024-01-17 15:44:35 +0100

    Update inscription files (Corentyn HAYER)
    Change some CSS parts in order to keep safe the View. Change the View in order to keep changes in the CSS file.

--- views/inscriptionView.php
+++ views/inscriptionView.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/css/global.css">
     <link rel="stylesheet" href="../assets/css/inscription.css">
-    <link rel="shortcut icon" href="../assets/img/logo.png" type="image/x-icon">
-    <title>Game Collection | Inscription</title>
+    <title>Inscription</title>
 </head>
 
 <body>
     <main>
         <div class="container">
-            <h1 class="heading1">Inscription</h1>
-            <form action="../controllers/inscriptionController.php" method="POST">
+            <h2 class="heading1">Inscription</h2>
+            <form action="../controllers/inscriptionController.php" method="post">
                 <label for="nom">Nom :</label>
-                <input type="text" name="nom" id="nom">
+                <input type="text" name="nom" required><br>
+
                 <label for="prenom">Prénom :</label>
-                <input type="text" name="prenom" id="prenom">
+                <input type="text" name="prenom" required><br>
+
                 <label for="email">Email :</label>
-                <input type="email" name="email" id="email">
-                <label for="password">Mot de passe :</label>
-                <input type="password" name="mdp" id="password">
-                <label for="confirm__password">Confirmation du mot de passe :</label>
-                <input type="password" name="mdp2" id="confirm__password">
+                <input type="email" name="email" required><br>
+
+                <label for="mot_de_passe">Mot de passe :</label>
+                <input type="password" name="mot_de_passe" required><br>
+
+                <label for="confirmation_mot_de_passe">Confirmer le mot de passe :</label>
+                <input type="password" name="confirmation_mot_de_passe" required><br>
+
                 <input type="submit" value="S'inscrire">
             </form>
         </div>
